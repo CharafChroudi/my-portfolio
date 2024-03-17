83583580700a102379e09b255613d2e76cc6e0c7
“Charaf <charafchroudi17@gmail.com>
2024-03-16 23:54:29 -0400

    base react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import reactDom from "react-dom/client";
+
+const root = reactDom.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <nav>Some nav links here</nav>
+    <main>This is my portfolio, I am Charaf</main>
+    <footer>This is how you find me</footer>
+  </>
+);
